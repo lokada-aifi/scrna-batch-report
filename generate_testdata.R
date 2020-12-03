f8b9cff2ad1374a81a970bbc0148ad6a18c326ef
Lauren Okada <lauren.okada@alleninstitute.org>
2020-12-03 11:52:17 -0800

    Updated report, update file structure, readme draft, testdata generation draft

---
+++ generate_testdata.R
@@ -0,0 +1,269 @@
+# Set Up
+library(stringr)
+library(H5weaver)
+
+# Make example files by reducing all data files to 1000 genes to speed up computation
+# time in development.
+# Based on B002 data
+
+# Source data: gs://private-analysis-store/B002-files
+# Saved all files to local directory
+
+
+
+source_data_dir <-
+mydir <- "/Users/lauren.okada/Packages/scrna-batch-report"
+outdir <- ""
+
+# Modify: B002 files
+#         Deidentify batch and sample names
+# Batch: T001
+# Samples: S01-S26
+
+all_labeled_h5 <- dir("/Users/lauren.okada/Projects/Exploration/B002/rna", 
+                      pattern = ".*_labeled.h5$",
+                      full.names = TRUE, 
+                      recursive = TRUE)
+all_labeled_h5
+# [1] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_IMM19-698_labeled.h5" 
+# [2] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_PB00004-01_labeled.h5"
+# [3] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_PB00006-01_labeled.h5"
+# [4] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_PB00010-01_labeled.h5"
+# [5] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_PB00011-01_labeled.h5"
+# [6] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_PB00012-01_labeled.h5"
+# [7] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_PB00013-01_labeled.h5"
+# [8] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_PB00014-01_labeled.h5"
+# [9] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_PB00015-01_labeled.h5"
+# [10] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_PB00016-01_labeled.h5"
+# [11] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_PB00017-01_labeled.h5"
+# [12] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_PB00018-01_labeled.h5"
+# [13] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P1/labeled_h5/B002-P1_PB00019-01_labeled.h5"
+# [14] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_IMM19-698_labeled.h5" 
+# [15] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_PB00020-01_labeled.h5"
+# [16] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_PB00021-01_labeled.h5"
+# [17] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_PB00022-01_labeled.h5"
+# [18] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_PB00024-01_labeled.h5"
+# [19] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_PB00053-01_labeled.h5"
+# [20] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_PB00055-01_labeled.h5"
+# [21] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_PB00056-01_labeled.h5"
+# [22] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_PB00057-01_labeled.h5"
+# [23] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_PB00059-01_labeled.h5"
+# [24] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_PB00060-01_labeled.h5"
+# [25] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_PB00061-01_labeled.h5"
+# [26] "/Users/lauren.okada/Projects/Exploration/B002/rna/B002-P2/labeled_h5/B002-P2_PB00062-01_labeled.h5"
+
+all_json <- dir("/Users/lauren.okada/Projects/Exploration/B002/rna/hto", 
+                pattern = "hto_processing_metrics.json",
+                full.names = TRUE, 
+                recursive = TRUE)
+all_json
+# [1] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C1W1_hto_processing_metrics.json"
+# [2] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C1W2_hto_processing_metrics.json"
+# [3] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C1W3_hto_processing_metrics.json"
+# [4] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C1W4_hto_processing_metrics.json"
+# [5] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C1W5_hto_processing_metrics.json"
+# [6] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C1W6_hto_processing_metrics.json"
+# [7] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C1W7_hto_processing_metrics.json"
+# [8] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C1W8_hto_processing_metrics.json"
+# [9] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C2W1_hto_processing_metrics.json"
+# [10] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C2W2_hto_processing_metrics.json"
+# [11] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C2W3_hto_processing_metrics.json"
+# [12] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C2W4_hto_processing_metrics.json"
+# [13] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2C2W5_hto_processing_metrics.json"
+# [14] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2C2W6_hto_processing_metrics.json"
+# [15] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2C2W7_hto_processing_metrics.json"
+# [16] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2C2W8_hto_processing_metrics.json"
+# [17] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2C3W1_hto_processing_metrics.json"
+# [18] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2C3W2_hto_processing_metrics.json"
+# [19] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2C3W3_hto_processing_metrics.json"
+# [20] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2C3W4_hto_processing_metrics.json"
+# [21] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2C3W5_hto_processing_metrics.json"
+# [22] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2C3W6_hto_processing_metrics.json"
+# [23] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2C3W7_hto_processing_metrics.json"
+# [24] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2C3W8_hto_processing_metrics.json"
+
+all_multiplet <- dir("/Users/lauren.okada/Projects/Exploration/B002/rna/hto",
+                     pattern = ".*multiplet.h5",
+                     full.names = TRUE, 
+                     recursive = TRUE)
+all_multiplet
+# [1] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1_multiplet.h5"
+# [2] "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2_multiplet.h5"
+
+all_control <- dir("/Users/lauren.okada/Projects/Exploration/controls",
+                   pattern = ".*labeled.h5",
+                   full.names = TRUE, 
+                   recursive = TRUE)
+all_control
+# [1] "/Users/lauren.okada/Projects/Exploration/controls/B001-P1_IMM19_709_labeled.h5"                               
+# [2] "/Users/lauren.okada/Projects/Exploration/controls/B002-P1_IMM19_698_labeled.h5"                               
+# [3] "/Users/lauren.okada/Projects/Exploration/controls/B002-P2_IMM19_698_labeled.h5"                               
+# [4] "/Users/lauren.okada/Projects/Exploration/controls/B004-P1_IMM19-695_labeled.h5"                               
+# [5] "/Users/lauren.okada/Projects/Exploration/controls/B004-P2_IMM19-695_labeled.h5"                               
+# [6] "/Users/lauren.okada/Projects/Exploration/controls/B010-P1_IMM19_394_2020-10-07T03:44:24.396314868Z_labeled.h5"
+# [7] "/Users/lauren.okada/Projects/Exploration/controls/B026-P1_IMM19_405_2020-10-10T20:09:23.161979427Z_labeled.h5"
+# [8] "/Users/lauren.okada/Projects/Exploration/controls/B026-P2_IMM19_405_2020-10-10T20:59:49.850564489Z_labeled.h5"
+# [9] "/Users/lauren.okada/Projects/Exploration/controls/B027-P1_IMM19_406_2020-09-29T00:27:07.534474463Z_labeled.h5"
+# [10] "/Users/lauren.okada/Projects/Exploration/controls/B027-P2_IMM19_406_2020-10-06T16:42:55.19194787Z_labeled.h5" 
+# [11] "/Users/lauren.okada/Projects/Exploration/controls/B033-P1_IMM19_408_2020-10-16T15:34:33.152518772Z_labeled.h5"
+# [12] "/Users/lauren.okada/Projects/Exploration/controls/B033-P2_IMM19_408_2020-10-17T18:48:16.001839234Z_labeled.h5"
+
+# well12_json <- "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C2W4_hto_processing_metrics.json"
+# json_file_list <- list(well1_json, well2_json, well3_json, well4_json, well5_json, well6_json,
+#                        well7_json, well8_json, well9_json, well10_json, well11_json, well12_json)
+# pool1_multiplets <- "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P1C1W1-RNA_multiplet.h5"
+# pool2_multiplets <- "/Users/lauren.okada/Projects/Exploration/B002/rna/hto/B002-P2_multiplet.h5"
+
+# De-identification IDs
+batch <- "T001"  
+samples <- paste0("S",stringr::str_pad(1:26, width = 2, pad = "0"))
+samples[c(1,14)] <- c("IMM00-002","IMM00-002")
+old_samples <- gsub(".*B002-P[12]_","",gsub("_labeled.h5","",all_labeled_h5))
+old_control_names <- gsub(".*B0\\d+-P[12]_","",gsub("_labeled.h5","",all_control))
+control_names <- paste0("IMM00-", stringr::str_pad(c(1,2,2,3,3,4,5,5,6,6,7,7), width = 3, pad = "0"))
+control_batch <- paste0("T", stringr::str_pad(c(0,1,1,2,2,3,4,4,5,5,6), width = 3, pad = "0"))
+old_control_batch <- gsub("-P[12].*","",gsub("_labeled.h5","",basename(all_control)))
+
+# Genes to keep
+chrM_genes <- fread(system.file("reference/GRCh38_10x_chrM_gene_metadata.csv.gz",
+                                package = "H5weaver"))
+
+genes <- c("CD3E", "CD4", "CD8A", "HLA-DRA", "CD14", "FCGR3A", "IL3RA", "ITGAX", 
+           "CD19", "MS4A1", "CD79A", "NCAM1", "NKG7", "PPBP", "IL7R")
+
+keep_genes <- c(chrM_genes$name, genes)
+n_random_genes <- 1000-length(keep_genes)
+
+#===============================================#
+# Modify labeled h5 by subsetting to 1000 genes
+#===============================================#
+# define gene subset, keeping all mt genes and genes of interest 
+sample1_list <- H5weaver::h5dump(all_labeled_h5[1])
+sample1_list <- h5_list_convert_to_dgCMatrix(sample1_list)
+
+set.seed(20201120)
+other_genes <- setdiff(sample1_list$matrix$features$name, keep_genes)
+gene_1k <- c(sample(other_genes, n_random_genes), keep_genes)
+
+# Modify each labeled h5 file
+for (i in seq_along(all_labeled_h5)){
+  sample_list <- h5dump(all_labeled_h5[[i]])
+  sample_list <- h5_list_convert_to_dgCMatrix(sample_list)
+  
+  # identify index of selected genes
+  i_gene_keep <- match(gene_1k, sample_list$matrix$features$name)
+  
+  # keep the selected genes in the count matrix
+  sample_list$matrix_dgCMatrix <- sample_list$matrix_dgCMatrix[i_gene_keep,]
+  
+  # keep only the selected genes as "features" in the h5
+  sample_list$matrix$features$name <- sample_list$matrix$features$name[i_gene_keep]
+  sample_list$matrix$features$genome <- sample_list$matrix$features$genome[i_gene_keep]
+  sample_list$matrix$features$feature_type <- sample_list$matrix$features$feature_type[i_gene_keep]
+  
+  # modify metadata to generic names (batch, ) and correct format
+  sample_list$matrix$observations$well_id <- gsub("-RNA","",gsub("B002", batch, sample_list$matrix$observations$well_id))
+  sample_list$matrix$observations$chip_id <- gsub("W.*","",sample_list$matrix$observations$well_id)
+  sample_list$matrix$observations$pool_id <- gsub("C.*","",sample_list$matrix$observations$chip_id)
+  sample_list$matrix$observations$batch_id <- batch
+  sample_list$matrix$observations$pbmc_sample_id <- samples[i]
+  
+  # return to original format and save new file
+  sample_list <- h5_list_convert_from_dgCMatrix(sample_list)
+  write_h5_list(sample_list,
+                file.path(mydir, sprintf("inst/extdata/labeled_h5/%s_%s_labeled.h5", batch, samples[i])),
+                overwrite = TRUE)
+  h5closeAll()
+}
+
+#===============================================#
+# Modify Multiplets to same 1000 genes
+#===============================================#
+for (i in seq_along(all_multiplet)){
+  multi_list <- h5dump(all_multiplet[i])
+  multi_list <- h5_list_convert_to_dgCMatrix(multi_list)
+  
+    # identify index of selected genes
+  i_gene_keep <- match(gene_1k, multi_list$matrix$features$name)
+  
+  # keep the selected genes in the count matrix
+  multi_list$matrix_dgCMatrix <- multi_list$matrix_dgCMatrix[i_gene_keep,]
+  
+  # keep only the selected genes as "features" in the h5
+  multi_list$matrix$features$name <- multi_list$matrix$features$name[i_gene_keep]
+  multi_list$matrix$features$genome <- multi_list$matrix$features$genome[i_gene_keep]
+  multi_list$matrix$features$feature_type <- multi_list$matrix$features$feature_type[i_gene_keep]
+  
+  # modify metadata to generic names (batch, ) and correct format
+  multi_list$matrix$observations$well_id <- gsub("-RNA","",gsub("B002", batch, multi_list$matrix$observations$well_id))
+  multi_list$matrix$observations$chip_id <- gsub("W.*","",multi_list$matrix$observations$well_id)
+  multi_list$matrix$observations$pool_id <- gsub("C.*","",multi_list$matrix$observations$chip_id)
+  multi_list$matrix$observations$batch_id <- batch
+  
+  # modify samples
+  multi_sample_list <- strsplit(multi_list$matrix$observations$pbmc_sample_id, split = ";" )
+  multi_sample_list_new <- lapply(multi_sample_list, function(x){
+    newnames <- samples[match(x, old_samples)]
+  })
+  multi_samples_new <- sapply(multi_sample_list_new, function(x){
+    paste(x, collapse = ";")
+  })
+  multi_list$matrix$observations$pbmc_sample_id <- multi_samples_new
+  
+  # return to original format and save new file
+  multi_list <- h5_list_convert_from_dgCMatrix(multi_list)
+  pool <- unique(multi_list$matrix$observations$pool_id)
+  write_h5_list(multi_list,
+                file.path(mydir, sprintf("inst/extdata/multiplet_h5/%s_multiplet.h5", pool)),
+                overwrite = TRUE)
+  h5closeAll()
+}
+
+#===============================================#
+# Modify Control h5 to same 1000 genes
+#===============================================#
+for (i in seq_along(all_control)){
+  control_list <- h5dump(all_control[i])
+  control_list <- h5_list_convert_to_dgCMatrix(control_list)
+  
+  # identify index of selected genes
+  i_gene_keep <- match(gene_1k, control_list$matrix$features$name)
+  
+  # keep the selected genes in the count matrix
+  control_list$matrix_dgCMatrix <- control_list$matrix_dgCMatrix[i_gene_keep,]
+  
+  # keep only the selected genes as "features" in the h5
+  control_list$matrix$features$name <- control_list$matrix$features$name[i_gene_keep]
+  control_list$matrix$features$genome <- control_list$matrix$features$genome[i_gene_keep]
+  control_list$matrix$features$feature_type <- control_list$matrix$features$feature_type[i_gene_keep]
+  
+  # modify metadata to generic names (batch, ) and correct format
+  control_list$matrix$observations$well_id <- gsub("-RNA","",gsub(old_control_batch[i], control_batch[i], control_list$matrix$observations$well_id))
+  control_list$matrix$observations$chip_id <- gsub("W.*","",control_list$matrix$observations$well_id)
+  control_list$matrix$observations$pool_id <- gsub("C.*","",control_list$matrix$observations$chip_id)
+  control_list$matrix$observations$batch_id <- control_batch[i]
+  control_list$matrix$observations$pbmc_sample_id <- control_names[i]
+  
+  # return to original format and save new file
+  control_list <- h5_list_convert_from_dgCMatrix(control_list)
+  write_h5_list(control_list,
+                file.path(mydir, sprintf("inst/extdata/control/%s_%s_labeled.h5", batch, control_names[i])),
+                overwrite = TRUE)
+  h5closeAll()
+}
+
+
+#===============================================#
+# Modify names in json files
+#===============================================#
+for (i in seq_along(all_json)){
+ json_list <- jsonlite::read_json(all_json[i])
+ 
+ json_list$well_id <- gsub("B002", batch, json_list$well_id)
+ 
+ # substitute all old sample names with new
+ names(json_list$pbmc_sample_hto_stats) <- samples[match(names(json_list$pbmc_sample_hto_stats), old_samples)]
+ 
+}
+  
+  
---
+++ run-scrna-batch-qc.R
@@ -0,0 +1,66 @@
+library(optparse)
+
+option_list <- list(
+  make_option(opt_str = c("-b","--batch_id"),
+              type = "character",
+              default = NULL,
+              help = "Batch identifier",
+              metavar = "character"),
+  make_option(opt_str = c("-i","--in_dir"),
+              type = "character",
+              default = NULL,
+              help = "Input directory containing h5 and json files",
+              metavar = "character"),
+  make_option(opt_str = c("-k","--in_key"),
+              type = "character",
+              default = NULL,
+              help = "Input sample sheet",
+              metavar = "character"),
+  make_option(opt_str = c("-d","--out_dir"),
+              type = "character",
+              default = NULL,
+              help = "Output Directory",
+              metavar = "character"),
+  make_option(opt_str = c("-o","--out_html"),
+              type = "character",
+              default = NULL,
+              help = "Output HTML run summary file",
+              metavar = "character")
+)
+
+opt_parser <- OptionParser(option_list = option_list)
+
+args <- parse_args(opt_parser)
+
+if(is.null(args$batch_id)) {
+  print_help(opt_parser)
+  stop("No parameters supplied.")
+}
+
+if(!dir.exists(args$out_dir)) {
+  dir.create(args$out_dir)
+}
+
+rmd_path <- file.path(args$out_dir,
+                      paste0(args$batch_id,
+                             "_scrna_batch_summary.Rmd"))
+
+# file.copy(system.file("rmarkdown/scrna_batch_summary.Rmd", package = "XXX"),
+#           rmd_path,
+#           overwrite = TRUE)
+
+file.copy("~/Packages/scrna-batch-report/scrna_batch_summary.Rmd",
+          rmd_path,
+          overwrite = TRUE)
+
+rmarkdown::render(
+  input = rmd_path,
+  params = list(batch   = args$batch_id,
+                in_dir  = args$in_dir,
+                in_key  = args$in_key,
+                out_dir = args$out_dir),
+  output_file = args$out_html,
+  quiet = TRUE
+)
+
+file.remove(rmd_path)
