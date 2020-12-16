931873415ad5bea16e8a425625ca4b3e46e87419
Lauren Okada <lauren.okada@alleninstitute.org>
2020-12-16 22:18:41 +0000

    fix control jackstraw cell subset  selection

--- test_run_hise.sh
+++ test_run_hise.sh
@@ -1,8 +1,8 @@
 #!/bin/bash/
 
 Rscript --vanilla \
-    /home/jupyter/scrna-batch-report/run-scrna-batch-qc.R \
-    -b T001 \
+    /home/jupyter/scrna-batch-report/run-scrna-batch-qc-hise.R \
     -d /home/jupyter/test_output/ \
-    -o  test.html
+    -o  test_B002P1_20201215.html \
+    2>&1 | tee /home/jupyter/test_output/test_20201215_log.txt
     
