1d208c5e8fcb9050d58b08e2617dc593b4f1ca6f
Lauren Okada <lauren.okada@alleninstitute.org>
2021-01-21 17:42:48 +0000

    update report formatting

--- test_run_hise.sh
+++ test_run_hise.sh
@@ -2,7 +2,10 @@
 
 Rscript --vanilla \
     /home/jupyter/scrna-batch-report/run-scrna-batch-qc-hise.R \
+    -b B001-P1-Test3  \
+    -i /home/jupyter/test_data/B002_3samples/ \
+    -k /home/jupyter/scrna-batch-report/inst/extdata/example_sample_key.csv  \
     -d /home/jupyter/test_output/ \
-    -o  test_B002P1_20201215.html \
-    2>&1 | tee /home/jupyter/test_output/test_20201215_log.txt
+    -o  B002-P1_3samples_20210105-2.html \
+    2>&1 | tee /home/jupyter/test_output/B002-P1_3samples_20210105-2_log.txt
     
