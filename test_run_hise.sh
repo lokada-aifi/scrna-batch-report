1e92e09dc8721192a4968348a91d926f75c50a9a
Lauren Okada <lauren.okada@alleninstitute.org>
2021-03-09 20:09:20 +0000

    Add hashing module messages

--- test_run_hise.sh
+++ test_run_hise.sh
@@ -7,6 +7,6 @@ Rscript --vanilla \
     -k /home/jupyter/test_data/B002_3samples/sample_key_B002_3samples.csv   \
     -c /home/jupyter/test_data/B002_3samples/default_rna_config_v1.csv  \
     -d /home/jupyter/test_output/ \
-    -o  B002-P1_3samples_20210305.html \
-    2>&1 | tee /home/jupyter/test_output/B002-P1_3samples_20210305-1_log.txt
+    -o  B002-P1_3samples_20210309.html \
+    2>&1 | tee /home/jupyter/test_output/B002-P1_3samples_20210309-1_log.txt
     
