e3343c087fc1f0e804cee7272ee27030be4a525a
Lauren Okada <lauren.okada@alleninstitute.org>
2021-03-30 15:33:30 +0000

    remove old files

--- test_run_hise.sh
+++ test_run_hise.sh
@@ -1,15 +1,15 @@
 #!/bin/bash/
 
 Rscript --vanilla \
-    /home/jupyter/scrna-batch-report/run-scrna-batch-qc.R \
+    /home/jupyter/batchreporter/run-scrna-batch-qc.R \
     -b B002  \
     -i /home/jupyter/test_data/B002_3samples/ \
     -k /home/jupyter/test_data/B002_3samples/sample_key_B002_3samples.csv   \
     -c /home/jupyter/test_data/B002_3samples/default_rna_config_v1.csv  \
     -d /home/jupyter/test_output/ \
-    -o  B002-P1_3samples_20210316.html \
+    -o  B002-P1_3samples.html \
     -n 14 \
-    -m 5000 \
-    2>&1 | tee /home/jupyter/test_output/B002-P1_3samples_20210316-1_log.txt
+    -m 20000 \
+    2>&1 | tee /home/jupyter/test_output/B002-P1_3samples_log.txt
 
     
